c4ac3589aba9ebac013683546f9673bd8d8e43f6
Mateskio <borokmate@gmail.com>
2026-04-01 23:20:16 +0200

    kinda finished

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ typedef struct
     Vector2 moveDir;
 } Snake;
 
-
+int apples = 0;
 
 void clear(){
     #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
@@ -35,12 +35,21 @@ void clear(){
 }
 
 void PrintMap(char *map, int width, int height){
-    clear();
+    for (int i = 0; i < width + 2; ++i)
+    {
+        printf("-");
+    }
+    printf("\n");
     for (int y = 0; y < height; y++){
+        printf("|");
         for (int x = 0; x < width; x++){
             printf("%c", map[y * width + x]);
         }
-        printf("\n");
+        printf("|\n");
+    }
+    for (int i = 0; i < width + 2; ++i)
+    {
+        printf("-");
     }
 }
 
@@ -48,18 +57,51 @@ int GetMapCoords(int x, int y, int width){
     return y * width + x;
 }
 
-int MoveSnake(char *map, Snake *snake, int width)
+int MoveSnake(char *map, Snake *snake, int width, int height, int *isCollision)
 {
-    int last_pos = snake->snake_body_pos[snake->head_pos_index]; 
+    int last_pos = snake->snake_body_pos[snake->head_pos_index];
+    int size = width * height;
     snake->head_pos_index = (snake->head_pos_index + 1) % snake->map_size;
-    snake->snake_body_pos[snake->head_pos_index] = last_pos + snake->moveDir.y * width + snake->moveDir.x * 1;
+    int old_x = ((last_pos) % width);
+    int new_x = ((last_pos) % width) + snake->moveDir.x;
+    Vector2 og_move = snake->moveDir;
+    if (old_x == width - 1 && new_x == width)
+    {
+        snake->moveDir.y = -1;
+    }
+    if (new_x == -1)
+    {
+        snake->moveDir.x = width - 1;
+    }
+    last_pos += snake->moveDir.x;
+    int new_y = last_pos + snake->moveDir.y * width;
+    if (new_y < 0)
+    {
+        new_y = size + new_y;
+    }
+    if (size - 1 < new_y)
+    {
+        new_y = 0 + (new_y - size);
+    }
+    int new_pos = new_y;
+    snake->moveDir = og_move;
+
+    snake->snake_body_pos[snake->head_pos_index] = new_pos;
+
     int isApple = map[snake->snake_body_pos[snake->head_pos_index]] == 'A';
-    if (map[snake->snake_body_pos[snake->tail_pos_index]] == 'A')
-        printf("WHYY\n");
+
     map[snake->snake_body_pos[snake->tail_pos_index]] = ' ';
     if (!isApple)
+    {
         snake->tail_pos_index = (snake->tail_pos_index + 1) % snake->map_size;
+    }
+
+    if (map[snake->snake_body_pos[snake->head_pos_index]] == 'S')
+    {
+        *isCollision = 1;
+    }
     map[snake->snake_body_pos[snake->head_pos_index]] = 'S';
+
     return isApple;
 }
 
@@ -68,9 +110,8 @@ void PlaceApple(char *map, int size)
     int random;
     do
     {
-        random = rand() % size - 1;
+        random = rand() % (size - 1);
     } while (map[random] == 'S');
-
     map[random] = 'A';
 }
 
@@ -78,15 +119,17 @@ void PlaceApple(char *map, int size)
 
 int main(){
     printf("Hello World\n");
-    int width = 5;
-    int heigth = 5;
+    int width = 20;
+    int heigth = 10;
     int size = width * heigth;
     char *map = malloc(size);
     memset(map, ' ', size);
     int *snake_pos = malloc(size * sizeof(int));
     memset(snake_pos, -1, size * sizeof(int));
     snake_pos[0] = 5;
-    map[GetMapCoords(3, 4, width)] = 'A';
+    srand(time(0));
+    PlaceApple(map, size);
+    int isCollision = 0;
 
     Snake snake = {
         .head_pos_index = 0,
@@ -96,27 +139,40 @@ int main(){
         .moveDir = {0, 0},
     };
     int prev_time = 0;
+    Vector2 snake_last_dir = snake.moveDir;
 
-    while (1){
-        if (_kbhit()) {
+    while (1)
+    {
+        if (_kbhit()) 
+        {
             char key = _getch();
-            if (key == 'w') snake.moveDir = (Vector2){0, -1};
-            if (key == 's') snake.moveDir = (Vector2){0, 1};
-            if (key == 'a') snake.moveDir = (Vector2){-1, 0};
-            if (key == 'd') snake.moveDir = (Vector2){1, 0};
+            if (key == 'w' && snake_last_dir.y !=  1) snake.moveDir = (Vector2){ 0, -1};
+            if (key == 's' && snake_last_dir.y != -1) snake.moveDir = (Vector2){ 0,  1};
+            if (key == 'a' && snake_last_dir.x !=  1) snake.moveDir = (Vector2){-1,  0};
+            if (key == 'd' && snake_last_dir.x != -1) snake.moveDir = (Vector2){ 1,  0};
         }
         
         if (prev_time != time(NULL))
         {
-            if (MoveSnake(map, &snake, width))
+            clear();
+            if (MoveSnake(map, &snake, width, heigth, &isCollision))
             {
                 PlaceApple(map, size);
+                ++apples;
             }
+            snake_last_dir = snake.moveDir;
+            printf("Apples eaten: %d\n", apples);
             PrintMap(map, width, heigth);
             prev_time = time(NULL);
         }
-        
+
+        if (isCollision)
+        {
+            break;
+        }
     }
+
+    printf("\nIt's over gang!\n");
 
     return 0;
 }
